3011d5721f605e2c271151f013a47e8d2bc8abef
krishnan1235 <133309868+krishnan1235@users.noreply.github.com>
2024-05-13 20:57:00 +0530

    Delete Phase_1 directory

--- Phase_1/Task_A_1.py
+++
@@ -1,9 +0,0 @@
-subject=['I','You']
-verb=['Play','Love']
-object1=['Cricket','Ludo']
-for i in subject:
-    for j in verb:
-        for k in object1:
-            print(i,j,k,sep=" ")
-            
-    
